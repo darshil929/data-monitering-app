4956f648c24dcdd418c7de4cb278ef66d665df6d
darshil929 <darshilt2001@gmail.com>
2023-06-22 15:00:01 +0530

    report feature added, csv excel works, pdf isnt working

--- client/src/components/Databases/Database1.jsx
+++ client/src/components/Databases/Database1.jsx
@@ -19,7 +19,62 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import * as XLSX from "xlsx";
+
 const Database1 = () => {
+
+    const downloadTable = (format) => {
+        const table = document.getElementById("table_with_data");
+        const tableData = Array.from(table.querySelectorAll("tr")).map((row) =>
+          Array.from(row.querySelectorAll("th, td")).map((cell) => cell.textContent)
+        );
+    
+        if (format === "csv") {
+          // Export to CSV
+          const csvData = tableData.map((row) => row.join(",")).join("\n");
+          const csvBlob = new Blob([csvData], { type: "text/csv" });
+          const csvUrl = URL.createObjectURL(csvBlob);
+          const downloadLink = document.createElement("a");
+          downloadLink.href = csvUrl;
+          downloadLink.download = "table.csv";
+          downloadLink.click();
+        } else if (format === "excel") {
+          // Export to Excel
+          const workbook = XLSX.utils.book_new();
+          const worksheet = XLSX.utils.aoa_to_sheet(tableData);
+          XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet 1");
+          const excelData = XLSX.write(workbook, {
+            type: "binary",
+            bookType: "xlsx",
+          });
+          const excelBlob = new Blob([s2ab(excelData)], {
+            type: "application/octet-stream",
+          });
+          const excelUrl = URL.createObjectURL(excelBlob);
+          const downloadLink = document.createElement("a");
+          downloadLink.href = excelUrl;
+          downloadLink.download = "table.xlsx";
+          downloadLink.click();
+        } else if (format === "pdf") {
+          // Export to PDF
+          console.log("fghjkl")
+          const doc = new jsPDF();
+          autoTable(doc, { html: "#table_with_data" });
+          doc.save("table.pdf");
+        }
+      };
+    
+      // Convert string to ArrayBuffer
+      const s2ab = (s) => {
+        const buf = new ArrayBuffer(s.length);
+        const view = new Uint8Array(buf);
+        for (let i = 0; i < s.length; i++) {
+          view[i] = s.charCodeAt(i) & 0xff;
+        }
+        return buf;
+      };
 
     const [open, setOpen] = React.useState(false);
     const [age, setAge] = React.useState('');
@@ -83,22 +138,6 @@ const Database1 = () => {
             <div className="graph-container">
                 <RealTimeDataChart ref={chartRef} />
             </div>
-            {/* <Tooltip title="Reset" arrow>
-                <Button variant="contained" className='graph-options' onClick={resetChartZoom}>
-                    <RotateLeftIcon></RotateLeftIcon>
-                </Button>
-            </Tooltip>
-            <Tooltip title="Zoom In" arrow>
-                <Button variant="contained" className='graph-options' onClick={zoomIn}>
-                    <ZoomInIcon></ZoomInIcon>
-                </Button>
-            </Tooltip>
-            <Tooltip title="Zoom Out" arrow>
-                <Button variant="contained" className='graph-options' onClick={zoomOut}>
-                    <ZoomOutIcon></ZoomOutIcon>
-                </Button>
-            </Tooltip> */}
-
             <div className='table-header flex'>
                 <h1>Tabular Data</h1>
                 <Tooltip title="Download Report" arrow>
@@ -110,9 +149,9 @@ const Database1 = () => {
                             {/* <DialogTitle>Export As</DialogTitle> */}
                             <DialogContent>
                                 <Box sx={{ display: 'flex' }}>
-                                <Button variant="contained" className='export-options'>PDF</Button>
-                                <Button variant="contained" className='export-options'>CSV</Button>
-                                <Button variant="contained" className='export-options'>Excel</Button>
+                                <Button variant="contained" onClick={() => downloadTable("pdf")} className='export-options'>PDF</Button>
+                                <Button variant="contained" onClick={() => downloadTable("csv")} className='export-options'>CSV</Button>
+                                <Button variant="contained" onClick={() => downloadTable("excel")} className='export-options'>Excel</Button>
                                 </Box>
                             </DialogContent>
                             <DialogActions>
@@ -122,8 +161,9 @@ const Database1 = () => {
                     </div>
                 </Tooltip>
             </div>
-
-            <Table />
+            <div id="table_with_data">
+                <Table/>
+            </div>
         </>
     );
 };

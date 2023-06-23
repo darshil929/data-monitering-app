c4caa4c0ecdd5d9332083133145e4f179aa2fc90
darshil929 <darshilt2001@gmail.com>
2023-06-23 13:51:18 +0530

    except pdf, everything is working in electron

--- client/src/components/Databases/Database1.jsx
+++ client/src/components/Databases/Database1.jsx
@@ -18,8 +18,62 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import * as XLSX from "xlsx";
 
 const Database1 = () => {
+
+  const downloadTable = (format) => {
+    const table = document.getElementById("table_with_data");
+    const tableData = Array.from(table.querySelectorAll("tr")).map((row) =>
+      Array.from(row.querySelectorAll("th, td")).map((cell) => cell.textContent)
+    );
+
+    if (format === "csv") {
+      // Export to CSV
+      const csvData = tableData.map((row) => row.join(",")).join("\n");
+      const csvBlob = new Blob([csvData], { type: "text/csv" });
+      const csvUrl = URL.createObjectURL(csvBlob);
+      const downloadLink = document.createElement("a");
+      downloadLink.href = csvUrl;
+      downloadLink.download = "table.csv";
+      downloadLink.click();
+    } else if (format === "excel") {
+      // Export to Excel
+      const workbook = XLSX.utils.book_new();
+      const worksheet = XLSX.utils.aoa_to_sheet(tableData);
+      XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet 1");
+      const excelData = XLSX.write(workbook, {
+        type: "binary",
+        bookType: "xlsx",
+      });
+      const excelBlob = new Blob([s2ab(excelData)], {
+        type: "application/octet-stream",
+      });
+      const excelUrl = URL.createObjectURL(excelBlob);
+      const downloadLink = document.createElement("a");
+      downloadLink.href = excelUrl;
+      downloadLink.download = "table.xlsx";
+      downloadLink.click();
+    } else if (format === "pdf") {
+      // Export to PDF
+      console.log("fghjkl")
+      const doc = new jsPDF();
+      autoTable(doc, { html: "#table_with_data" });
+      doc.save("table.pdf");
+    }
+  };
+
+  // Convert string to ArrayBuffer
+  const s2ab = (s) => {
+    const buf = new ArrayBuffer(s.length);
+    const view = new Uint8Array(buf);
+    for (let i = 0; i < s.length; i++) {
+      view[i] = s.charCodeAt(i) & 0xff;
+    }
+    return buf;
+  };
 
   const [open, setOpen] = React.useState(false);
   const [age, setAge] = React.useState('');
@@ -93,9 +147,9 @@ const Database1 = () => {
               {/* <DialogTitle>Export As</DialogTitle> */}
               <DialogContent>
                 <Box sx={{ display: 'flex' }}>
-                  <Button variant="contained" className='export-options'>PDF</Button>
-                  <Button variant="contained" className='export-options'>CSV</Button>
-                  <Button variant="contained" className='export-options'>Excel</Button>
+                  <Button variant="contained" onClick={() => downloadTable("pdf")} className='export-options'>PDF</Button>
+                  <Button variant="contained" onClick={() => downloadTable("csv")} className='export-options'>CSV</Button>
+                  <Button variant="contained" onClick={() => downloadTable("excel")} className='export-options'>Excel</Button>
                 </Box>
               </DialogContent>
               <DialogActions>
@@ -105,7 +159,9 @@ const Database1 = () => {
           </div>
         </Tooltip>
       </div>
-      <Table />
+      <div id="table_with_data">
+        <Table />
+      </div>
     </>
   );
 };
